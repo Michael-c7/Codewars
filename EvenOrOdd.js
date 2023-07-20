9463245474c52e477c3c707238fc5e1b59275baf
Michael Carr <mcarr2468@gmail.com>
2023-07-19 20:22:52 -0500

    add EvenOrOdd

---
+++ EvenOrOdd.js
@@ -0,0 +1,11 @@
+// Standard / ES5 version
+// function evenOrOdd(number) {
+//   if(number % 2 === 0) {
+//     return "Even"
+//   } else {
+//     return "Odd"
+//   }
+// }
+
+// one line version / ES6 version
+const evenOrOdd = (number) => number % 2 === 0 ? "Even" : "Odd"
